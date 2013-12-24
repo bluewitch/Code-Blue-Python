9b0c3888912b6da4c4632da2623075c4eef9444d
Douglas Kuhn <bluewitchproductions@gmail.com>
2013-12-23 17:37:17 -0800

    Create fizzbuzz.py
    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+# Python: fizzbuzz.py
+
+	import sys
+	for i in range(-50, 100):
+	if i%3==0:
+		sys.stdout.write('Fizz')
+	if i%5==0:
+		sys.stdout.write('Buzz')
+	if (i%5<>0 and i%3<>0):
+		print i,
+	print
