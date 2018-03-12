ebf7dda5bc8a8c1773f2e277c70b5711319abefa
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-03-12 13:44:43 -0700

    Create scratchpad.py

---
+++ scratchpad.py
@@ -0,0 +1,40 @@
+
+import re
+import os
+import urllib
+
+splat_data = 
+"""
+
+"""
+
+github_urls =[{'Douglas Kuhn','https://github.com/bluewitch'},
+			  {'',''},
+
+]
+
+ewallet_urls = {'https://myetherwallet.github.io/'}
+
+blockchain_urls = [{'Reatime Blockchain Monitor', 'https://blocks.wizb.it/'},
+				   {'Crypto Currecny Activity', 'https://coincheckup.com/analysis/github'},
+				   {'',''},
+                   {'https://blockstack.org/', ''},
+                   ]
+
+Person = 
+
+{"Name":"",
+ "":""
+		  }
+
+
+hall_of_heros =[ {'Naval Ravikant', 'https://twitter.com/naval',''},
+                 {'Oriol Vinyals', 'https://twitter.com/OriolVinyalsML', 'http://bit.ly/2p4TWJW'},
+                 {'Andrew Trask', 'https://twitter.com/iamtrask',''},
+                 {'Balaji Srinivasan ','https://twitter.com/balajis','http://bit.ly/2FIcbyx'},
+                 {'Bryan Johnson ','https://twitter.com/bryan_johnson',''},
+                 {'','',''},
+                 {'','',''},
+                 {'','',''},
+
+                 ]
