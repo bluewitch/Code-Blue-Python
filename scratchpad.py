09c0ce8135724ba52c04d563252189b1dbc79dc1
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-03-12 22:23:15 -0700

    Update scratchpad.py

--- scratchpad.py
+++ scratchpad.py
@@ -2,33 +2,40 @@
 import re
 import os
 import urllib
+import csv
+
+
+with open('names.csv', 'r') as csv_file:
+	csv_reader = csv.reader(csv_file)
+
+	for line in csv_reader:
+		print
+
+
+csv_data =
+"""
+
+"""
 
 splat_data = 
 """
+Ok, this is demonstration code for simple Python functions.
 
+The first Object is called the hall_of_heros which holds 
+the name, twitter address, media, github account
+
+The second is called the github_urls which holds a name and a 
+github url of user
 """
 
-github_urls =[{'Douglas Kuhn','https://github.com/bluewitch'},
-			  {'',''},
-
-]
-
-ewallet_urls = {'https://myetherwallet.github.io/'}
-
-blockchain_urls = [{'Reatime Blockchain Monitor', 'https://blocks.wizb.it/'},
-				   {'Crypto Currecny Activity', 'https://coincheckup.com/analysis/github'},
-				   {'',''},
-                   {'https://blockstack.org/', ''},
-                   ]
-
-Person = 
-
-{"Name":"",
- "":""
-		  }
-
-
-hall_of_heros =[ {'Naval Ravikant', 'https://twitter.com/naval',''},
+#Hall of Heros (A.I./M.L.)
+# ~name
+# ~twitter account
+# ~media link (youtube, etc.)
+# ~github account
+hall_of_heros =[ 
+				 {'Name','Twitter URL','Github Account','Media URL'}
+				 {'Naval Ravikant', 'https://twitter.com/naval',''},
                  {'Oriol Vinyals', 'https://twitter.com/OriolVinyalsML', 'http://bit.ly/2p4TWJW'},
                  {'Andrew Trask', 'https://twitter.com/iamtrask',''},
                  {'Balaji Srinivasan ','https://twitter.com/balajis','http://bit.ly/2FIcbyx'},
@@ -38,3 +45,21 @@ hall_of_heros =[ {'Naval Ravikant', 'https://twitter.com/naval',''},
                  {'Trent McConaghy','','http://bit.ly/2FDXzwz'}
 
                  ]
+
+github_urls =[{'Douglas Kuhn','https://github.com/bluewitch'},
+			  {'Juan Benet','https://github.com/jbenet'},
+			  {'',''},
+
+
+]
+
+ewallet_urls = {'https://myetherwallet.github.io/'}
+
+# blockchain_urls, 
+# Holds the name and url to important web resources
+
+blockchain_urls = [{'Reatime Blockchain Monitor', 'https://blocks.wizb.it/'},
+				   {'Crypto Currecny Activity', 'https://coincheckup.com/analysis/github'},
+				   {'',''},
+                   {'https://blockstack.org/', ''},
+                   ]
