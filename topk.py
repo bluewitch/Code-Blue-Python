53a2f08cea36347cdcad567cbb897ffb4be02657
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-08-21 14:40:54 -0700

    Update topk.py

--- topk.py
+++ topk.py
@@ -74,4 +74,4 @@ Python
         count = collections.Counter(words)
         heap = [(-freq, word) for word, freq in count.items()]
         heapq.heapify(heap)
-        return [heapq.heappop(heap)[1] for _ in xrange(k)]
+        return [heapq.heappop(heap)[1] for _ in range(k)]
