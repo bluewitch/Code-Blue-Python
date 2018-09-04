19f3a01dd29b9e6fb3b12b3fd07553f4126616da
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-09-04 12:20:35 -0700

    Create HR_solveMeFirst.py

---
+++ HR_solveMeFirst.py
@@ -0,0 +1,14 @@
+#HR_solveMeFirst.py
+
+def solveMeFirst(a,b)
+	#Hint: Type return a+b below
+	return a+b
+	
+#Gotcha!
+#You must cast your parameters as int
+#or else they will concatenate as defult 			
+#string instead of defult integer add sum
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)
