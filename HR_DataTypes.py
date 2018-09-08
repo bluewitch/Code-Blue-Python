919cc402d7cd0cbc0b0d0c4e996ab645af8199bd
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-09-08 10:21:07 -0700

    Update HR_DataTypes.py

--- HR_DataTypes.py
+++ HR_DataTypes.py
@@ -4,9 +4,9 @@ s = 'HackerRank'
 
 
 # Declare second integer, double, and String variables.
-i2 = int(12)
-d2 = float(4.0)
-s2 = str('is the best place to learn and practice coding!')
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
 
 # Read and save an integer, double, and String to your variables.
 
