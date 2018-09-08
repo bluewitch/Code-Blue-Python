00ba0184b1014874d794fa7a5495bed8dde9d959
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-09-08 10:11:54 -0700

    Create HR_DataTypes.py

---
+++ HR_DataTypes.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank'
+
+
+# Declare second integer, double, and String variables.
+i2 = int(12)
+d2 = float(4.0)
+s2 = str('is the best place to learn and practice coding!')
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+i2)
+# Print the sum of the double variables on a new line.
+print(d+d2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+s2)
