e7faeed609fa1c1e926113d121c2deb85c437161
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-09-09 09:08:12 -0700

    Create HR_arithmeticOperators.py

---
+++ HR_arithmeticOperators.py
@@ -0,0 +1,7 @@
+# arithmeticOperators.py
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Elegant solution, one line
+    print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
