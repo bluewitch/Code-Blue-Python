43864819f4b7635efcd5a7a3eded02ad765eefb8
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-09-09 08:24:07 -0700

    Create HR_pythonIfElse.py

---
+++ HR_pythonIfElse.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+n = int(input())
+"""
+Given an integer, perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+"""
+check = {True: "Not Weird", False: "Weird"}
+print(check[ n%2==0 and (n in range(2,6) or n > 20)])
