fedb30553a35d2b9de61355d3d430f9cbfbf86e3
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-09-10 09:11:40 -0700

    Create HR_pythonCountSubstring.py

---
+++ HR_pythonCountSubstring.py
@@ -0,0 +1,3 @@
+def count_substring(string, sub_string):
+    # List Comprehension
+    return(sum([1 for i in range(0, len(string) - len(sub_string) + 1) if (string[i:(len(sub_string)+i)] == sub_string)]))
