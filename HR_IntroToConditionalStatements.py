be3428c9fe6de7741cec7f3899bcc71049b113ca
Douglas Kuhn <bluewitchproductions@gmail.com>
2018-09-11 17:56:51 -0700

    Create HR_IntroToConditionalStatements.py

---
+++ HR_IntroToConditionalStatements.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    #N = int(input())
+    # One liner in a lambda function
+    print((lambda N:'Weird' if N % 2 else 'Not Weird')(int(input())))
