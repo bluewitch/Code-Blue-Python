0499670dc6ace1e5ae3c4fc1f67593c1f4e6efe0
Douglas Kuhn <bluewitchproductions@gmail.com>
2019-03-31 15:48:59 -0700

    Update HR_pythonCountSubstring.py
    Updated comment

--- HR_pythonCountSubstring.py
+++ HR_pythonCountSubstring.py
@@ -1,3 +1,7 @@
 def count_substring(string, sub_string):
+    """
+    This is a simple function that returns a List Comprehension to find the substrings within character strings, 
+    outputing the total number of occurrences of the substring in the original string
+    """
     # List Comprehension
     return(sum([1 for i in range(0, len(string) - len(sub_string) + 1) if (string[i:(len(sub_string)+i)] == sub_string)]))
