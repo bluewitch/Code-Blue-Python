5555119066f970a68b9ef5ea18a74adb6b400b2a
Douglas Kuhn <bluewitchproductions@gmail.com>
2019-03-31 15:28:29 -0700

    Update HR_pythonStringSplitAndJoin.py
    Updated description of function

--- HR_pythonStringSplitAndJoin.py
+++ HR_pythonStringSplitAndJoin.py
@@ -1,4 +1,9 @@
 def split_and_join(line):
+    """
+    Split and join is a simple function that illustrates some string manipulation by creating a variable named line, 
+    using the embedded .split() method, to convert to a list of strings and interposing a - character by way of 
+    the .join method, and returning the value of the line variable.
+    """
     # write your code here
     line = line.split(" ") # a is converted to a list of strings.
     line = "-".join(line)
